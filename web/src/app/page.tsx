e8ed4e63a52c98aadd1f25240d8a2300dec34877
Erick M <erickmoreta1221@gmail.com>
2026-01-03 08:00:23 -0400

    Fix: Restore 'use client' directive in Landing Page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { ShoppingBasket, ChefHat, Wallet, TrendingUp, Users, ArrowRight, Sparkles, ScanBarcode, Zap } from 'lucide-react';
